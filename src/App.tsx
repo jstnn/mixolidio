669ab7949946e3fe1b7b12acda2182c1a5a0f14a
Nicolas Justiniano <njustini@thoughtworks.com>
2021-06-22 10:25:03 -0400

    [@jstnn] backup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import icon from '../assets/icon.svg';
-import './App.global.css';
+// import icon from '../assets/icon.svg';
+// import './App.global.css';
+import ProjectList from "./components/ProjectList";
 
 const Hello = () => {
+  const [files, setFiles] = useState<File[]>([])
+
+  useEffect(() => {
+    const holder = document.getElementById('drag-file');
+
+    if (!holder) return;
+ 
+    holder.ondragover = () => {
+        return false;
+    };
+
+    holder.ondragleave = () => {
+        return false;
+    };
+
+    holder.ondragend = () => {
+        return false;
+    };
+
+    holder.ondrop = (e: DragEvent) => {
+      e.preventDefault();
+      const files = e?.dataTransfer?.files ?? {} as FileList
+      const poolFiles: File[] = [];
+      Array.from(files).forEach(file => poolFiles.push(file))
+      if (poolFiles.length>0) setFiles(poolFiles);
+      return false;
+    };
+  }, [])
+
   return (
     <div>
-      <div className="Hello">
+      {/* <div className="Hello">
         <img width="200px" alt="icon" src={icon} />
+      </div> */}
+      <h1>Projects</h1>
+      <div id="drag-file">
+        <p>Drag your files here</p>
+        {files.map(file => (
+          file.path
+        ))}
       </div>
-      <h1>electron-react-boilerplate</h1>
+      <ProjectList />
       <div className="Hello">
         <a
           href="https://electron-react-boilerplate.js.org/"
